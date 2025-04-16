5a512f698fd400c87f732f677e431734af3a6116
Anders Kagervall <anders.kagervall@gmail.com>
2025-04-16 15:49:34 +0200

    Check for readxl and usethis in package_data.R

--- data-raw/package_data.R
+++ data-raw/package_data.R
@@ -2,6 +2,12 @@
 ## Script that creates data that should be installed as package data. Run this script
 ## to save data as .rda in ../data
 ## All data should be documented in ../R/package_data.R
+if (requireNamespace("readxl", quietly = TRUE) &&
+    requireNamespace("usethis", quietly = TRUE)) {
+  message("readxl and usethis are available")
+} else {
+  stop("readxl and usethis packages are required to run this script.")
+})
 
 WGBAST_rivers <- readxl::excel_sheets("data-raw/WGBAST_sites.xlsx")
 WGBAST_sites <- lapply(WGBAST_rivers, function(r) {
